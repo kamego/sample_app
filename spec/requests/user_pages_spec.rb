12b2baf95141489e21f66288b288f96b64d30395
Kamil Kiedrowski <kamego@gmail.com>
2012-03-01 13:29:16 -0600

    finish chapter 7 excercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,24 +23,41 @@ describe "User pages" do
 
     before { visit signup_path }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button "Sign up" }.not_to change(User, :count)
-      end
-    end
-
-    describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+      describe "with invalid information" do
+        it "should not create a user" do
+          expect { click_button "Sign up" }.not_to change(User, :count)
+        end
       end
 
-      it "should create a user" do
-        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+
+        describe "after saving the user" do
+          before { click_button "Sign up" }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.flash.success', text: 'Welcome') }
+        end
+
+        it "should create a user" do
+          expect { click_button "Sign up" }.to change(User, :count).by(1)
+        end
       end
-    end
+
+      describe "error messages" do
+        before { click_button "Sign up" }
+
+        let(:error) { 'errors prohibited this user from being saved' }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content(error) }
+      end
   end
 end
 
